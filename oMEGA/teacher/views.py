be3eff4566540cf05ea67fe6e7ccfe9c51532391
Raphael Knaus <rknuus@gmail.com>
2017-06-05 17:10:41 +0200

    replace template by shortcut

--- oMEGA/teacher/views.py
+++ oMEGA/teacher/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse, Http404
-from django.template import loader
 from django.shortcuts import render
 from .models import Exam
 
 
 def index(request):
     exams = Exam.objects.all()
-    template = loader.get_template('exams/index.html')
     context = {'exams': exams}
-    return HttpResponse(template.render(context, request))
-    # return render(request, 'exams/index.html', context)
+    return render(request, 'exams/index.html', context)
 
 
 def exam(request, exam_id):
